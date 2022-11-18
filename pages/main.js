9ad2d3291a35fee3acc0373563f8e015da8e04ff
Maantje Mol <35200312+maantjemol@users.noreply.github.com>
2022-11-18 20:29:58 +0100

    cookies🍪

--- pages/main.js
+++ pages/main.js
@@ -5,3 +5,24 @@ document.getElementById("addButton").addEventListener("click", function () {
   count++;
   text.innerHTML = count;
 });
+
+let headersList = {
+  Accept: "*/*",
+  "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+  "Content-Type": "application/json",
+};
+
+let bodyContent = JSON.stringify({
+  username: "maantje",
+  password: "lolhaha",
+});
+
+fetch("https://localhost:1111/api/login", {
+  method: "POST",
+  body: bodyContent,
+  headers: headersList,
+})
+  .then((response) => response.text())
+  .then((data) => {
+    console.log(data);
+  });
--- server.py
+++ server.py
@@ -1,81 +1,18 @@
-# importeert een hoop shit
-import socket, ssl, glob
-#import os 
+from server_lib import *
+import json
 
-# Maakt alvast routes list aan voor later (scroll naar beneden voor spoilers)
 routes = []
-
-class Response():
-    # Maakt het makkelijk om een responce te bouwen
-    def __init__(self, status:int, data:str, contentType:str = "text/html"):
-        self.status = status
-        self.data = data
-        self.contentType = contentType
-
-    # Bouwt een HTTP message om te versturen, In dit geval in de vorm:
-    # 
-    # HTTP/1.1 200 OK
-    # Server: Your_Mom
-    # Content-type: text/html; charset=UTF-8
-    # "Eventuele data"
-    # 
-    def build(self):
-        response = f"HTTP/1.1 {self.status} OK\nServer: Your_Mom\nContent-type: {self.contentType}; charset=UTF-8\n\r\n\r{self.data}\n\r\n\r"
-        return response
-
-
-class Request():
-    # Maakt een request bruikbaar, inclusief headers
-    def __init__(self, request:bytes):
-
-        self.headers = {}
-        self.request_string = request.decode()
-        self.method = self.request_string.split(" ")[0]
-        self.url = self.request_string.split(" ")[1]
-
-        # split header string en stopt het in een dict voor gebruik
-        lines = self.request_string.split('\n',1)[1]
-        for line in lines.split("\n")[0:-2]:
-            words = line.split(":", 1)
-            self.headers[words[0]] = words[1].replace("\n", "").replace("\r", "")
-
-
-class Route():
-    # Maakt een route aan
-    def __init__(self, webpath:str, localpath:str, contentType:str = "text/html"):
-        self.webpath = webpath
-        self.localpath = localpath
-        self.contentType = contentType
-    
-    # Bouwt een response voor de route, als in een HTTP message
-    def build(self):
-        try:
-            # Probeert het HTML bestand te zoeken
-            file = open(self.localpath).read()
-            response = Response(200, file, self.contentType).build()
-            return response
-        except Exception as e:
-            print(f"Error: {e}")
-            # Als het HTML bestand niet gevonden kan worden stuurt ie een 404 not found
-            response = Response(404, "Not found").build()
-            return response
-
+cookies = [{"Uwu":"test"}]
 
 def NewRoute(webpath, localpath, contentType = "text/html"):
-    # Voegt gewoon een route toe aan een lijst maar dit ziet er clean uit.
-    # Route(webpath, localpath) maakt een route, met een webpath in de vorm
-    # van /test.html(voorbeeld) en een localpath naar het bestand ./pages/test.html(voorbeeld)
-
     routes.append(
         Route(webpath, localpath, contentType)
     )
 
 
-def InitializeRoutes():
-    # Maakt een lijst aan met alle bestanden in de map pages, dat doet FindFiles()
-    files:list = FindFiles("pages")
 
-    # Loopt door alle files heen en maakt een route
+def InitializeRoutes():
+    files:list = FindFiles("pages")
     for filepath in files:
         filepath = filepath.replace("\\", "/")
         route = "/" + filepath.split("/", 2)[2]
@@ -99,29 +36,26 @@ def InitializeRoutes():
 
         else:
             NewRoute(route, filepath)
-
     
-    # Voegt een route toe aan de lijst routes 
     NewRoute("/", "./pages/index.html")
     NewRoute("/testpagina", "./pages/index.html")
 
 
-def FindFiles(folder):
-    # Maakt een lijst van alle bestanden in een map
+def handleLogin(request):
+    resp = {
+        "tesasdfast": "uwu"
+    }
 
-    files = []
-    for f in glob.glob(f'./{folder}/**/*.*', recursive=True):
-        files.append(f)
-    return files
+    cookies.append({"sadfasdf":"test"})
+
+    return resp
 
 
-def FindRoute(routes, url):
-    # Zoekt in de routes en kijkt of er een route is die overeen komt met de url
-
-    for route in routes:
-        if route.webpath == url:
-            return route
-    return Route("/", "./pages/404.html")
+def ApiRoutes():
+    routes.append(Apiroute(
+        webpath="/api/login",
+        responseFunc=handleLogin
+    ))
 
 
 class HTTP_Server():
@@ -159,19 +93,18 @@ class HTTP_Server():
             route = FindRoute(self.routes, request.url)
 
             # Maakt er een mooi HTTP objectje van en flikkert die terug naar je browser
-            response =  route.build()
+            if type(route) == type(Apiroute("t", None)): #Api route
+                response = route.build(request)
+            else:
+                response =  route.build(cookies)
+            
             connstream.sendall(response.encode())
             connstream.close()
 
 
 
 if __name__ == "__main__":
-    # We maken hier de routes aan om te gebruiken voor de webserver.
-    # Denk hierbij zegmaar aan https://localhost:1111/test.html
-    # Hier is /test.html de route
     InitializeRoutes()
-
-    # Hier starten we de server op https://localhost:1111
+    ApiRoutes()
     server = HTTP_Server("localhost", 1111, routes)
     server.start()
-    
