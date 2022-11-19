7845f8c96d711bb2f0ce7ac9884ca0ca55d34844
Maantje Mol <35200312+maantjemol@users.noreply.github.com>
2022-11-19 21:44:56 +0100

    Added login logic

---
+++ pages/login/login.js
@@ -0,0 +1,48 @@
+email_text = document.getElementById("email");
+password_text = document.getElementById("password");
+
+const checkInputs = () => {};
+
+// button.onclick = async () => {};
+
+async function submitForm(e) {
+  e.preventDefault();
+  const submit_button = document.getElementById("submit_button");
+  submit_button.disabled = true;
+
+  let headersList = {
+    "Content-Type": "application/json",
+  };
+
+  let bodyContent = JSON.stringify({
+    username: email_text.value,
+    password: password_text.value,
+  });
+
+  let response = await fetch("/api/login", {
+    method: "POST",
+    body: bodyContent,
+    headers: headersList,
+  });
+
+  if (response) {
+    submit_button.disabled = false;
+  }
+
+  if (response.status === 200) {
+    let data = await response.json();
+
+    if (data.error) {
+      console.log(data.error);
+      return;
+    }
+
+    document.cookie = `id=${data.cookie}`;
+  }
+}
+
+const form = document.getElementById("formmmm");
+
+form.addEventListener("submit", async function (e) {
+  await submitForm(e);
+});
